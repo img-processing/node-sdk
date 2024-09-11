596d089a631affbed5972786570bbb39219af75c
Rolando Andrade <rolandoandradefernandez@gmail.com>
2024-09-11 08:31:42 -0400

    feat: finished transformation endpoints

--- src/api-client.ts
+++ src/api-client.ts
@@ -30,13 +30,13 @@ export class IMGProcessingClient {
     }
   }
 
-  protected async imageRequest(call: () => ResponsePromise<ImageObject>): Promise<ImageObject> {
+  protected async imageRequest<Format extends ImageObject.SupportedFormat = ImageObject.SupportedFormat>(call: () => ResponsePromise<ImageObject>): Promise<ImageObject<Format>> {
     const response = await this.request(call);
-    return new ImageObject({ image: response, client: this });
+    return new ImageObject({ image: response, client: this }) as ImageObject<Format>;
   }
 
 
-  async uploadImage({ image, name }: IMGProcessingClient.uploadImage.Options): Promise<ImageObject> {
+  async uploadImage({ image, name }: IMGProcessingClient.uploadImage.Params): Promise<ImageObject> {
     const formData = new FormData();
     formData.append('name', name);
 
@@ -63,10 +63,64 @@ export class IMGProcessingClient {
     }));
   }
 
-  async createImageFromUrl({ url, name }: IMGProcessingClient.createImageFromUrl.Options): Promise<ImageObject> {
+  async createImageFromUrl({ url, name }: IMGProcessingClient.createImageFromUrl.Params): Promise<ImageObject> {
     return this.imageRequest(() => this.client.post<ImageObject>('v1/images', {
       json: {
         url,
+        name
+      }
+    }));
+  }
+
+  /**
+   * Create a new image by converting an existing image to a different format.
+   *
+   * The supported image formats are `jpeg`, `png`, and `webp`.
+   *
+   * - **JPEG (Joint Photographic Experts Group):** A commonly used method of lossy compression for digital images,
+   * particularly for those images produced by digital photography. JPEG compression significantly reduces the file size, but it can also reduce the image quality.
+   * - **PNG (Portable Network Graphics):** A raster-graphics file format that supports lossless data compression.
+   * PNG is often used for images that require transparency or when the image quality must be preserved without any loss.
+   * - **WebP:** A modern image format that provides superior lossless and lossy compression for images on the web.
+   * WebP images are smaller compared to JPEG and PNG, while maintaining similar or better image quality.
+   */
+  async convert<Format extends ImageObject.SupportedFormat>({ imageId, format, name }: IMGProcessingClient.convert.Params<Format>): Promise<ImageObject<Format>> {
+    return this.imageRequest(() => this.client.post<ImageObject<Format>>(`v1/images/${imageId}/convert`, {
+      json: {
+        format,
+        name
+      }
+    }));
+  }
+
+  /**
+   * Crop an image by specifying the dimensions of the crop area.
+   *
+   * The crop area is defined by 2 points: the top-left corner at `(x1, y1)` and the bottom-right corner at `(x2, y2)`.
+   */
+  async crop({ imageId, x1, y1, x2, y2, name }: IMGProcessingClient.crop.Params): Promise<ImageObject> {
+    return this.imageRequest(() => this.client.post<ImageObject>(`v1/images/${imageId}/crop`, {
+      json: {
+        x1,
+        y1,
+        x2,
+        y2,
+        name
+      }
+    }));
+  }
+
+  /**
+   * Mirror an existing image horizontally or vertically to create a new image.
+   *
+   * Mirror an image horizontally means that the image is mirrored along the vertical axis, while flipping an
+   * image vertically means that the image is mirrored along the horizontal axis. You can also mirror an image
+   * horizontally and vertically at the same time using the mode `both`.
+   */
+  async mirror({ imageId, mode, name }: IMGProcessingClient.mirror.Params): Promise<ImageObject> {
+    return this.imageRequest(() => this.client.post<ImageObject>(`v1/images/${imageId}/mirror`, {
+      json: {
+        mode,
         name
       }
     }));
@@ -86,12 +140,27 @@ export class IMGProcessingClient {
    * Additionally, you can specify the background color for the letterbox when using the `contain` fit mode, and
    * the gravity for cropping or positioning the image when using the `cover` and `contain` fit modes.
    */
-  async resizeImage({ imageId, width, height, fit }: IMGProcessingClient.resizeImage.Options): Promise<ImageObject> {
+  async resize({ imageId, width, height, fit, name, position }: IMGProcessingClient.resize.Params): Promise<ImageObject> {
     return this.imageRequest(() => this.client.post<ImageObject>(`v1/images/${imageId}/resize`, {
       json: {
         width,
         height,
-        fit
+        fit,
+        name,
+        position
+      }
+    }));
+  }
+
+  /**
+   * Rotate an existing image by a specified angle.
+   */
+  async rotate({ imageId, angle, unit, name }: IMGProcessingClient.rotate.Params): Promise<ImageObject> {
+    return this.imageRequest(() => this.client.post<ImageObject>(`v1/images/${imageId}/rotate`, {
+      json: {
+        angle,
+        unit,
+        name
       }
     }));
   }
@@ -124,21 +193,21 @@ export declare namespace IMGProcessingClient {
 
   export namespace uploadImage {
     type FilePath = string;
-    export type Options = {
+    export type Params = {
       image: Blob | File | FilePath | Buffer;
       name: string;
     }
   }
 
   export namespace createImageFromUrl {
-    export type Options = {
+    export type Params = {
       url: string;
       name: string;
     }
   }
 
-  export namespace resizeImage {
-    export type Options = {
+  export namespace resize {
+    export type Params = {
       /** The ID of the image to resize. */
       imageId: string;
       /** The desired width of the resized image in pixels. */
@@ -158,6 +227,71 @@ export declare namespace IMGProcessingClient {
       letterbox_color?: string;
       /** The position of the image when using the `cover` or `contain` fit modes. */
       position?: 'center' | 'top' | 'right' | 'bottom' | 'left' | 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right';
+      /** The name of the image. If not provided, the original image name will be used. */
+      name?: string;
+    }
+  }
+
+  export namespace crop {
+    export type Params = {
+      /** The ID of the image to crop. */
+      imageId: string;
+      /** The x-coordinate of the top-left corner of the crop area. */
+      x1: number;
+      /** The y-coordinate of the top-left corner of the crop area. */
+      y1: number;
+      /** The x-coordinate of the bottom-right corner of the crop area. */
+      x2: number;
+      /** The y-coordinate of the bottom-right corner of the crop area. */
+      y2: number;
+      /** The name of the image. If not provided, the original image name will be used. */
+      name?: string;
+    }
+  }
+
+  export namespace mirror {
+    /**
+     * The mirror mode to apply to the image.
+     * - `horizontal`: Mirror the image along the vertical axis.
+     * - `vertical`: Mirror the image along the horizontal axis.
+     * - `both`: Mirror the image along both axes.
+     */
+    export type Mode = 'horizontal' | 'vertical' | 'both';
+    export type Params = {
+      /** The ID of the image to mirror. */
+      imageId: string;
+      /** /*** The mirror mode to apply to the image. */
+      mode: Mode;
+      /** The name of the image. If not provided, the original image name will be used. */
+      name?: string;
+    }
+  }
+
+  export namespace convert {
+    export type Params<Format extends ImageObject.SupportedFormat> = {
+      /** The ID of the image to convert. */
+      imageId: string;
+      /** The format to convert the image to. */
+      format: Format;
+      /** The quality of the converted image if the target format is `jpeg`. */
+      quality?: number;
+      /** The name of the image. If not provided, the original image name will be used. */
+      name?: string;
+    }
+  }
+
+  export namespace rotate {
+    export type Params = {
+      /** The ID of the image to rotate. */
+      imageId: string;
+      /** The angle in degrees or radians rotate the image. */
+      angle: number;
+      /** The unit of the angle. Default is `degrees`. */
+      unit?: 'degrees' | 'radians';
+      /** The background color to fill the empty areas after rotating the image. Default is `#000000`. */
+      background_color?: string;
+      /** The name of the image. If not provided, the original image name will be used. */
+      name?: string;
     }
   }
 }
--- src/image-object.ts
+++ src/image-object.ts
@@ -6,20 +6,20 @@ import type { WithoutImageId } from './types.js';
  * information about the image, such as its URL, size, and format. The Image object is returned in the response body of
  * all image processing requests.
  */
-export class ImageObject implements ImageObject.Image {
+export class ImageObject<Format extends ImageObject.SupportedFormat = ImageObject.SupportedFormat> implements ImageObject.Image<Format> {
   public readonly id: `image_${string}`;
   public readonly name: string;
   public readonly url: string | null;
   public readonly width: number;
   public readonly height: number;
-  public readonly format: ImageObject.SupportedFormat;
+  public readonly format: Format;
   public readonly size: number;
   public readonly created_at: string;
 
   /** The client used to interact with the IMG Processing API. */
   protected readonly client: IMGProcessingClient;
 
-  constructor({ image, client }: ImageObject.ConstructorProps) {
+  constructor({ image, client }: ImageObject.ConstructorProps<Format>) {
     this.id = image.id;
     this.name = image.name;
     this.url = image.url;
@@ -29,6 +29,59 @@ export class ImageObject implements ImageObject.Image {
     this.size = image.size;
     this.created_at = image.created_at;
     this.client = client;
+  }
+
+  /**
+   * Create a new image by converting an existing image to a different format.
+   *
+   * The supported image formats are `jpeg`, `png`, and `webp`.
+   * - **JPEG (Joint Photographic Experts Group):** A commonly used method of lossy compression for digital images,
+   * particularly for those images produced by digital photography.
+   * JPEG compression significantly reduces the file size, but it can also reduce the image quality.
+   * - **PNG (Portable Network Graphics):** A raster-graphics file format that supports lossless data compression.
+   * PNG is often used for images that require transparency or when the image quality must be preserved without any loss.
+   * - **WebP:** A modern image format that provides superior lossless and lossy compression for images on the web.
+   * WebP images are smaller compared to JPEG and PNG, while maintaining similar or better image quality.
+   */
+  async convert({ format, quality, name }: ImageObject.convert.Params): Promise<ImageObject> {
+    return await this.client.convert({
+      imageId: this.id,
+      format,
+      quality,
+      name
+    });
+  }
+
+  /**
+   * Crop an image by specifying the dimensions of the crop area.
+   *
+   * The crop area is defined by 2 points: the top-left corner at `(x1, y1)` and the bottom-right corner at `(x2, y2)`.
+   */
+  async crop({ x1, y1, x2, y2, name }: ImageObject.crop.Params): Promise<ImageObject> {
+    return await this.client.crop({
+      imageId: this.id,
+      x1,
+      y1,
+      x2,
+      y2,
+      name
+    });
+  }
+
+  /**
+   * Mirror an existing image horizontally or vertically to create a new image.
+   *
+   * Mirror an image horizontally means that the image is mirrored along the vertical axis, while flipping an
+   * image vertically means that the image is mirrored along the horizontal axis.
+   * You can also mirror an image
+   * horizontally and vertically at the same time using the mode `both`.
+   */
+  async mirror({ mode, name }: IMGProcessingClient.mirror.Params): Promise<ImageObject> {
+    return await this.client.mirror({
+      imageId: this.id,
+      mode,
+      name
+    });
   }
 
   /**
@@ -45,10 +98,23 @@ export class ImageObject implements ImageObject.Image {
    * Additionally, you can specify the background color for the letterbox when using the `contain` fit mode, and
    * the gravity for cropping or positioning the image when using the `cover` and `contain` fit modes.
    */
-  async resize(options: ImageObject.resize.Options): Promise<ImageObject.Image> {
-    return await this.client.resizeImage({
+  async resize(options: ImageObject.resize.Params): Promise<ImageObject> {
+    return await this.client.resize({
       imageId: this.id,
       ...options
+    });
+  }
+
+  /**
+   * Rotate an existing image by a specified angle.
+   */
+  async rotate({ angle, unit, name, background_color }: ImageObject.rotate.Params): Promise<ImageObject> {
+    return await this.client.rotate({
+      imageId: this.id,
+      angle,
+      unit,
+      name,
+      background_color
     });
   }
 }
@@ -61,7 +127,7 @@ export declare namespace ImageObject {
    * information about the image, such as its URL, size, and format. The Image object is returned in the response body of
    * all image processing requests.
    */
-  export type Image = {
+  export type Image<Format extends SupportedFormat = SupportedFormat> = {
     /** The unique identifier of the image. This identifier is used to reference the image in subsequent requests.*/
     id: `image_${string}`;
     /** The name of the image. This name is provided when uploading the image and is the way
@@ -77,20 +143,32 @@ export declare namespace ImageObject {
     /** The height of the image in pixels. */
     height: number;
     /** The format of the image. */
-    format: SupportedFormat;
+    format: Format;
     /** The estimated size of the image in bytes. The size is an estimate and may not be exact since images can be compressed or optimized depending
      on the format and quality settings used during processing. */
     size: number;
     /** The date and time when the image was created. The date and time are in ISO 8601 format. */
     created_at: string;
   }
-  export type ConstructorProps = {
+  export type ConstructorProps<Format extends SupportedFormat = SupportedFormat> = {
     /** The IMG Processing API image object. */
-    image: Image;
+    image: Image<Format>;
     /** The client used to interact with the IMG Processing API. */
     client: IMGProcessingClient;
   }
   export namespace resize {
-    export type Options = WithoutImageId<IMGProcessingClient.resizeImage.Options>;
+    export type Params = WithoutImageId<IMGProcessingClient.resize.Params>;
+  }
+  export namespace crop {
+    export type Params = WithoutImageId<IMGProcessingClient.crop.Params>;
+  }
+  export namespace convert {
+    export type Params = WithoutImageId<IMGProcessingClient.convert.Params>;
+  }
+  export namespace mirror {
+    export type Params = WithoutImageId<IMGProcessingClient.mirror.Params>;
+  }
+  export namespace rotate {
+    export type Params = WithoutImageId<IMGProcessingClient.rotate.Params>;
   }
 }
