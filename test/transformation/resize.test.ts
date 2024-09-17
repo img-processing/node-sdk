f7054f12c708c1a0aab7c9e9b38ed4a6ea1d1aad
Rolando Andrade <rolandoandradefernandez@gmail.com>
2024-09-17 16:00:42 -0400

    feat: transformation tests 2

---
+++ test/transformation/resize.test.ts
@@ -0,0 +1,25 @@
+import { beforeAll, describe, expect, test } from 'vitest';
+import { ImageObject, IMGProcessingClient } from '../../src/index.js';
+import { getApiKey } from '../helpers.js';
+
+describe('resize', () => {
+  const client: IMGProcessingClient = new IMGProcessingClient({ apiKey: getApiKey() });
+  let uploadedImage: ImageObject;
+
+  beforeAll(async () => {
+    const url = "https://storage.img-processing.com/og-image.jpg";
+    uploadedImage = await client.createImageFromUrl({ url, name: "test_image" });
+  })
+
+  test('should resize the image', async () => {
+    const modulatedImage = await uploadedImage.resize({
+      width: 500,
+      height: 500,
+      fit: 'contain',
+      letterbox_color: 'red',
+    });
+    expect(modulatedImage).toBeDefined();
+    expect(modulatedImage.id).toMatch(/^image_[a-zA-Z0-9]{24}$/);
+    expect(modulatedImage.name).toBe("test_image");
+  })
+})
---
+++ test/transformation/rotate.test.ts
@@ -0,0 +1,24 @@
+import { beforeAll, describe, expect, test } from 'vitest';
+import { ImageObject, IMGProcessingClient } from '../../src/index.js';
+import { getApiKey } from '../helpers.js';
+
+describe('rotate', () => {
+  const client: IMGProcessingClient = new IMGProcessingClient({ apiKey: getApiKey() });
+  let uploadedImage: ImageObject;
+
+  beforeAll(async () => {
+    const url = "https://storage.img-processing.com/og-image.jpg";
+    uploadedImage = await client.createImageFromUrl({ url, name: "test_image" });
+  })
+
+  test('should rotate an image', async () => {
+    const rotatedImage = await uploadedImage.rotate({
+      angle: 90,
+    });
+    expect(rotatedImage).toBeDefined();
+    expect(rotatedImage.id).toMatch(/^image_[a-zA-Z0-9]{24}$/);
+    expect(rotatedImage.name).toBe("test_image");
+    expect(rotatedImage.width).toBe(uploadedImage.height);
+    expect(rotatedImage.height).toBe(uploadedImage.width);
+  })
+})
